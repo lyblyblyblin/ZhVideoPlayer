e681a8f1c90109caf29d258ac13e14687d2988f2
lyblyblyblin@live.com <linyb@DESKTOP-NPCE5E7.localdomain>
2018-03-29 20:08:45 +0800

    改成适合老人用的视频播放器

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
-# ZhVideoPlayer 这是一个Android视频播放器项目
+源项目地址https://github.com/JustRight815/ZhVideoPlayer
 
+Android视频播放器项目
 
+目标人群
+不认识字的，眼睛不好的老人
 
-目前的功能包括：获取本地视频资源，播放器的界面风格是仿老版慕课app的视频播放器，都是采用的Vitamio视频播放库，支持小屏播放、全屏播放、快进、快退、声音调节、
-亮度调节。如果想播放在线资源，只需传入在线视频url即可。
+特色，播放视频前通过tts播放视频名称
 
-通过该项目可以学习到的内容有：沉浸式状态栏、动态权限获取、仿qq下拉刷新，快速回到视频列表顶部，本地视频资源获取，获取视频资源的缩略图，DiskLruCache和自定义imageloader、手势控制快进快退等。    
-
-
-项目效果如下：    
-
-<img width="300" height="450" src="https://github.com/JustRight815/ZhVideoPlayer/blob/master/screenshots/6.png"/><img width="300" height="450" src="https://github.com/JustRight815/ZhVideoPlayer/blob/master/screenshots/7.png"/><img width="300" height="450" src="https://github.com/JustRight815/ZhVideoPlayer/blob/master/screenshots/2.jpg"/><img width="300" height="450" src="https://github.com/JustRight815/ZhVideoPlayer/blob/master/screenshots/5.png"/><img width="300" height="450" src="https://github.com/JustRight815/ZhVideoPlayer/blob/master/screenshots/4.jpg"/><img width="450" height="300" src="https://github.com/JustRight815/ZhVideoPlayer/blob/master/screenshots/1.jpg"/><img width="450" height="300" src="https://github.com/JustRight815/ZhVideoPlayer/blob/master/screenshots/8.jpg"/><img width="450" height="300" src="https://github.com/JustRight815/ZhVideoPlayer/blob/master/screenshots/9.jpg"/>
+更改内容
+1，修复权限问题
+2，改变寻找视频文件方法
+3，数据库记录视频路径信息等
+4，加入等待动画
+5，加入tts
+6，更改图标
+7，删去认为无用功能
+8，一处异步显示图片有bug，找不到原因，注释掉
+9，第一次点击变色
--- app/src/main/java/com/zh/zhvideoplayer/player/LocalVideoListAdapter.java
+++ app/src/main/java/com/zh/zhvideoplayer/player/LocalVideoListAdapter.java
@@ -3,6 +3,7 @@ package com.zh.zhvideoplayer.player;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
+import android.graphics.Color;
 import android.media.MediaMetadataRetriever;
 import android.media.ThumbnailUtils;
 import android.provider.MediaStore.Video.Thumbnails;
@@ -14,96 +15,134 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.zh.zhvideoplayer.R;
-import com.zh.zhvideoplayer.bean.LocalVideoBean;
-import com.zh.zhvideoplayer.util.StringUtils;
 
-import java.util.List;
+import java.util.ArrayList;
+
+import static com.zh.zhvideoplayer.TTSTool.ttsToSpe;
+
 /**
  * 本地视频列表的适配器
- * 
  */
 public class LocalVideoListAdapter extends BaseAdapter {
 
-	private List<LocalVideoBean> mVideoList;
-	private Context mContext;
-	private LocalVideoImageLoader mVideoImageLoader;
-	
-	
-	public LocalVideoListAdapter(List<LocalVideoBean> videoList, Context context) {
-		super();
-		this.mVideoList = videoList;
-		this.mContext = context;
-		mVideoImageLoader = new LocalVideoImageLoader(context);// 初始化缩略图载入方法
-	}
+    private Context mContext;
+    private LocalVideoImageLoader mVideoImageLoader;
+    private int defaultSelection = -1;
+    private ArrayList<String> paths;
+    private ArrayList<String> names;
 
-	@Override
-	public int getCount() {
-		return mVideoList.size();
-	}
+    public LocalVideoListAdapter(Context context, ArrayList<String> names, ArrayList<String> paths,boolean ttsiswork) {
+        super();
+        this.paths = paths;
+        this.names = names;
+        this.mContext = context;
+        mVideoImageLoader = new LocalVideoImageLoader(context);// 初始化缩略图载入方法
+    }
 
-	@Override
-	public LocalVideoBean getItem(int position) {
-		return mVideoList.get(position);
-	}
+    /**
+     * @param position 设置高亮状态的item
+     */
+    public void setSelectPosition(int position) {
+        if (!(position < 0 || position > paths.size())) {
+            defaultSelection = position;
+            notifyDataSetChanged();
+        }
+    }
+    /**
+     * @param position 删除item
+     */
+    public void removePosition(int position) {
+        if (!(position < 0 || position > paths.size())) {
+            paths.remove(position);
+            names.remove(position);
+            notifyDataSetChanged();
+        }
+    }
+    /**
+     * @param position speak 信息
+     */
+    public void setSpeakPosition(int position) {
+        if (!(position < 0 || position > paths.size())) {
+            ttsToSpe(mContext,names.get(position));
+        }
+    }
 
-	@Override
-	public long getItemId(int position) {
-		return position;
-	}
 
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		ViewHolder viewHolder;
-		if(convertView == null){
-			convertView = LayoutInflater.from(mContext).inflate(R.layout.video_local_video_item, null);
-			viewHolder = new ViewHolder(convertView);
-			convertView.setTag(viewHolder);			
-		} else{
-			viewHolder = (ViewHolder) convertView.getTag();
-		}
-		viewHolder.setData(mVideoList.get(position));
-		viewHolder.image.setTag(mVideoImageLoader.hashKeyForDisk(mVideoList.get(position).path));//绑定imageview
-		mVideoImageLoader.showThumbByAsynctack(mVideoList.get(position).path, viewHolder.image);
-		return convertView;
-	}
-	
-	private static class ViewHolder{
-		private ImageView image;
-		private TextView title;
-		private TextView duration;
-		Bitmap bitmap = Bitmap.createBitmap( 10,10, Config.ARGB_4444 );
-		
-		public ViewHolder(View view){
-			image = (ImageView) view.findViewById(R.id.image);
-			title = (TextView) view.findViewById(R.id.title);
-			duration = (TextView) view.findViewById(R.id.duration);
-		}
-		
-		public void setData(LocalVideoBean video){
-			title.setText("名称：" + video.title);
-			duration.setText("时长：" + StringUtils.generateTime(Long.parseLong(video.duration)));
-//			sizeText.setText("大小：" + StringUtils.generateFileSize(video.size));
-			image.setImageBitmap(bitmap);
-		}
-		
-		/**
-		 * 获取视频文件截图
-		 * 
-		 * @param path
-		 *            视频文件的路径
-		 * @return Bitmap 返回获取的Bitmap
-		 */
-		public static Bitmap getVideoThumb(String path) {
-			MediaMetadataRetriever media = new MediaMetadataRetriever();
-			media.setDataSource(path);
-			return media.getFrameAtTime();
-		}
-		
-		public static Bitmap getVideoThumb1(String path) {
-			Bitmap  bitmap = ThumbnailUtils.createVideoThumbnail(path,
-	                   Thumbnails.MICRO_KIND);//MINI_KIND: 512 x 384，MICRO_KIND: 96 x 96,
+
+    @Override
+    public int getCount() {
+        return paths.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.video_local_video_item, null);
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+        viewHolder.setData(names.get(position), paths.get(position));
+        viewHolder.image.setTag(mVideoImageLoader.hashKeyForDisk(paths.get(position)));//绑定imageview
+        mVideoImageLoader.showThumbByAsynctack(paths.get(position), viewHolder.image);
+
+
+        if (position == defaultSelection) {// 选中时设置单纯颜色
+            convertView.setBackgroundColor(Color.parseColor("#80CBC4"));
+        } else {// 未选中时设置selector
+            convertView.setBackgroundColor(Color.parseColor("#FFFFFF"));
+        }
+        return convertView;
+    }
+
+    private static class ViewHolder {
+        private View view;
+        private ImageView image;
+        private TextView title;
+        Bitmap bitmap = Bitmap.createBitmap(10, 10, Config.ARGB_4444);
+
+        public ViewHolder(View view) {
+            this.view = view;
+            image = (ImageView) view.findViewById(R.id.image);
+            title = (TextView) view.findViewById(R.id.title);
+
+        }
+
+        public void setData(String videoname, String videopath) {
+            title.setText(videoname);
+            image.setImageResource(R.drawable.loading_11);
+        }
+
+        /**
+         * 获取视频文件截图
+         *
+         * @param path 视频文件的路径
+         * @return Bitmap 返回获取的Bitmap
+         */
+        public static Bitmap getVideoThumb(String path) {
+            MediaMetadataRetriever media = new MediaMetadataRetriever();
+            media.setDataSource(path);
+            return media.getFrameAtTime();
+        }
+
+        public static Bitmap getVideoThumb1(String path) {
+            Bitmap bitmap = ThumbnailUtils.createVideoThumbnail(path,
+                    Thumbnails.MICRO_KIND);//MINI_KIND: 512 x 384，MICRO_KIND: 96 x 96,
 //			bitmap = ThumbnailUtils.extractThumbnail(bitmap, width, height, ThumbnailUtils.OPTIONS_RECYCLE_INPUT);
-			return bitmap;
-		}
-	}
+            return bitmap;
+        }
+    }
 }
